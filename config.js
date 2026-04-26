3857c52b182e6b25a4a342861de6e29b910e1b87
Deepesh2507 <deepeshbatra589@gmail.com>
2026-04-26 13:28:18 +0530

    chore: update FAIRLENS_API_BASE to point to new deployment URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Set this to your deployed backend URL before publishing frontend.
 // Example:
 // window.FAIRLENS_API_BASE = "https://fairlens-api.onrender.com";
-window.FAIRLENS_API_BASE = "https://fairlens-api.onrender.com";
+window.FAIRLENS_API_BASE = "https://fairlens-api-489l.onrender.com";
